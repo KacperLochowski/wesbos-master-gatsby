1dad8e30c11bf957b7e5f9d92f9a14226329dbbc
Kacper Łochowski <klochowski2802@gmail.com>
2021-01-30 23:40:15 +0100

    feat: add all required pages

---
+++ gatsby/src/pages/pizzas.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const PizzasPage = () => <div>Pizzas Page</div>
+
+export default PizzasPage
---
+++ gatsby/src/pages/slicemasters.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const SlicemastersPage = () => <div>Slice Masters Page</div>
+
+export default SlicemastersPage
