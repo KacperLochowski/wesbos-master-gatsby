4b043695c146d039cc9f63b3f8bf0dda4ea3cc5f
Kacper Łochowski <klochowski2802@gmail.com>
2021-02-03 21:53:36 +0100

    feat: add global styles with styled components

--- gatsby/src/components/layout.jsx
+++ gatsby/src/components/layout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import 'normalize.css'
+import GlobalStyles from '../assets/styles/globalStyles'
+
 import Nav from './nav'
 import Footer from './footer'
 
 const Layout = ({ children }) => (
   <div>
+    <GlobalStyles />
     <Nav />
     {children}
     <Footer />
