67724deac619bac4f7ddfdbb94bde921e62ee309
Kacper Łochowski <klochowski2802@gmail.com>
2021-02-08 13:18:08 +0100

    styles: style layout component

--- gatsby/src/components/layout.tsx
+++ gatsby/src/components/layout.tsx
@@ -1,18 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
 
+// Styles
 import 'normalize.css'
+import Typography from '../assets/styles/typography'
 import GlobalStyles from '../assets/styles/globalStyles'
 
+// Images
+import stripes from 'src/assets/images/stripes.svg'
+
+// Components
 import Nav from './nav'
 import Footer from './footer'
-import Typography from '../assets/styles/typography'
+
+const SiteBorderStyles = styled.div`
+  max-width: 1000px;
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw, 12rem);
+  background: white url(${stripes});
+  background-size: 1500px;
+  padding: 5px;
+  padding: clamp(5px, 1vw, 25px);
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.044);
+  border: 5px solid white;
+  @media (max-width: 1100px) {
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
+`
+
+const ContentStyles = styled.div`
+  background: white;
+  padding: 2rem;
+`
 
 const Layout: React.FC = ({ children }) => (
   <>
     <Typography />
     <GlobalStyles />
-    <Nav />
-    {children}
+    <SiteBorderStyles>
+      <ContentStyles>
+        <Nav />
+        {children}
+      </ContentStyles>
+    </SiteBorderStyles>
     <Footer />
   </>
 )
